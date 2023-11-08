385e405c1e79485b47dbddbacaa451477858e363
Chinyere Ottah <ottahc@mcmaster.ca>
2023-11-08 06:47:00 -0500

    I created a package called surfaceR

--- R/surfaceR-package.R
+++ R/surfaceR-package.R
@@ -4,3 +4,33 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+#' elevation_raster: A package that is helpful for creating surface features such as slope and aspect for geospatial analysis using `R`.
+
+#'
+#' This package provides functions for generating surface features like slope and aspect from digital elevation models. It is designed for geospatial analysis in R.
+#'
+#' @import sp
+#' @importFrom raster raster
+
+#'
+
+#' @docType package
+#' @name elevation_raster
+#' @author Chinyere Ottah, School of Earth, Environment and Society, McMaster University \email{ottahc@@mcmaster.ca}
+#' @references \url{https://github.com/p/ChinyereRuth/surfaceR/}
+NULL
+
+##############################################
+##                DATA SETS                 ##
+##############################################
+
+## 2023 Digital elevation data of North Western Territory
+
+
+#'
+#' A digital elevation model of NWT showing areas of high and low elevation. This project was created to see how areas of low and high elevation, and also places where the slope is steep contributes to fire database with 101 indicators aggregated to NUTS 2 statistical level. Data were collected from open sources by
+#'  The sources where this data was gotten was from the USGS website. Link: https://search.earthdata.nasa.gov/search/granules?projectId=2435656526 include data from the fields of education (ETER, Erasmus), science (USPTO, MA-Graph, GRID),
+
+#'
+#' @format A GeoTIFF File
